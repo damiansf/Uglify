c22f2e98ca9d182b2415bad6bd48796fbb9e3a28
damiansf <damiansandhu11@gmail.com>
2020-06-01 00:13:38 -0400

    creating extension

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Uglify
+
+An app that makes your browsing experince extremly pai.... unique.
+
+Available in the Chrome Webstore: 
---
+++ roll.js
@@ -0,0 +1,3 @@
+if(Math.floor(Math.random() * 6) + 1  === 4) {
+    document.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+}
